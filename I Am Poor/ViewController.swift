ca5748bfa05a29a6bfa877aba924cdb222332715
Vladislav Reznik <vladek_1999@mail.ru>
2023-03-26 21:55:17 +0300

    add assets, setup UI

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Georgia-Italic", size: 25)
+        label.textColor = .red
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "I Am Poor"
+        return label
+    }()
+    
+    private lazy var coalImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "coal")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
+}
 
-
+private extension ViewController {
+    func setupUI() {
+        view.backgroundColor = .blue
+        view.addSubview(titleLabel)
+        view.addSubview(coalImageView)
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            coalImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            coalImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            coalImageView.widthAnchor.constraint(equalToConstant: 300),
+            coalImageView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
 }
 
